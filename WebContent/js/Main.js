9d0b032379723ea05c171de48f768775be95ec35
Caitlin Donahue <caitlindonahue95@gmail.com>
2017-05-24 23:56:20 -0500

    Made the game variable global so it can be tested with the console

--- WebContent/js/Main.js
+++ WebContent/js/Main.js
@@ -1,15 +1,18 @@
+//global variable for the sake of testing
+var gameGlobal;
+
 window.onload = function() {
 	// Create your Phaser game and inject it into an auto-created canvas.
 	// We did it in a window.onload event, but you can do it anywhere (requireJS
 	// load, anonymous function, jQuery dom ready, - whatever floats your boat)
-	var game = new Phaser.Game(1920, 1080, Phaser.AUTO);
+	gameGlobal = new Phaser.Game(1920, 1080, Phaser.AUTO);
 
 	// Add the States your game has.
-	game.state.add("Boot", Boot);
-	game.state.add("Preload", Preload);
+	gameGlobal.state.add("Boot", Boot);
+	gameGlobal.state.add("Preload", Preload);
 	
 	// Now start the Boot state.
-	game.state.start("Boot");
+	gameGlobal.state.start("Boot");
 };
 
 
